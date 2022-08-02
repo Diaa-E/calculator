b5c219326edcd49616d10609955dd1673aa7e3c1
Diaa-E <blarb21@yahoo.com>
2022-08-02 06:05:03 +0200

    Add expression evaluation

--- script.js
+++ script.js
@@ -2,21 +2,96 @@
 
 let expression = "";
 
+//number button event
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(button => button.addEventListener('click', (e) => {
     write(e.target.getAttribute('data-value'));
 }));
 
+//operation button event
 const operationButtons = document.querySelectorAll(".operation");
 operationButtons.forEach(button => button.addEventListener('click', (e) => {
     write(e.target.getAttribute('data-value'));
 }));
 
+//clear button event
 const clearButton = document.querySelector(".clear");
 clearButton.addEventListener('click', clear);
 
+//delete button event
 const delButton = document.querySelector(".delete");
 delButton.addEventListener('click', del)
+
+//evaluate button event
+const eqlButton = document.querySelector('.equals');
+eqlButton.addEventListener('click', () => {
+    evaluateExpression()
+    console.log("event works")
+});
+
+
+function evaluateExpression()
+{
+    //catch if the last entered item is an operation
+    if (expression.charAt(expression.length -1) === " ")
+    {
+        console.log("Syntax Error: Each operator must have 2 operands");
+        return
+    }
+
+    //each operation is sandwitched by a space on each side
+    //!!!!caution: string ending with delimeter adds an empty element to the end of the array
+    const expressionStack = expression.split(" "); 
+    let opStack = [];
+    let numStack = [];
+
+    while (expressionStack.length > 0)
+    {
+        if (expressionStack[expressionStack.length -1] === '+' 
+            || expressionStack[expressionStack.length -1] === '-'
+            || expressionStack[expressionStack.length -1] === '*'
+            || expressionStack[expressionStack.length -1] === '/')
+        {
+            if (opStack.length > 0 && 
+                evaluatePrecedence(opStack[opStack.length -1]) 
+                >= evaluatePrecedence(expressionStack[i]))
+            {
+                numStack.push(operate(numStack.pop(), numStack.pop(), opStack.pop()));
+                opStack.push(expressionStack.pop());
+            }
+            else
+            {
+                opStack.push(expressionStack.pop());
+            }
+        }
+        else
+        {
+            numStack.push(+expressionStack.pop());
+            console.log(numStack)
+        }
+    }
+
+    while (opStack.length > 0)
+    {
+        numStack.push(operate(numStack.pop(), numStack.pop(), opStack.pop()));
+    }
+
+    expression = numStack.pop();
+    updateDisplay();
+}
+
+function evaluatePrecedence(operation)
+{
+    switch (operation)
+    {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+    }
+}
 
 function updateDisplay()
 {
@@ -37,8 +112,16 @@ function clear()
 
 function del()
 {
-    expression = expression.slice(0, -1);
-    updateDisplay();
+    if (expression.charAt(expression.length -1) === " ")
+    {
+        expression = expression.slice(0, -3); //remove the operation and its surrounding space
+        updateDisplay();
+    }
+    else
+    {
+        expression = expression.slice(0, -1);
+        updateDisplay();
+    }
 }
 
 function operate(value1, value2, operation)
